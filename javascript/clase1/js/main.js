55b43e85c986e1c3a4d6426562d2edc93c820f3c
micaela-marrero <micaela.marrero@globant.com>
2024-10-17 15:58:50 +0200

    Primero clase

---
+++ javascript/clase1/js/main.js
@@ -0,0 +1,50 @@
+// este es un comentario
+
+/* Este es un comentario
+de mas lineas
+*/
+
+
+//declaracion
+
+let edad = 28; //variable que puede cambiar su valor
+let nombre;
+
+const anioDeNacimiento = 1996; //variable que no cambia su valor
+const apellido = "Marrero";
+
+
+//valores
+//numer: un valor numerico que puede ser enterno o decimal
+//string
+
+
+nombre = "Micaela";
+
+
+console.log(apellido)
+
+
+
+//OPERACIONES CON VARIABLES NUMERICAS
+
+
+const numero1 = 10;
+const numero2 = 40;
+
+const resultadoSuma = numero1 + numero2;
+
+const resultadoResta = numero1 - numero2;
+
+const resultadoMultiplicacion = numero1 * numero2;
+
+const resultadoDivision = numero1 / numero2;
+
+
+//OPERACIONES CON VARIABLES STRING
+
+const cadena1 = "Hola";
+const cadena2 = "Saludos";
+
+const saludo = cadena1 + " " + cadena2
+
